33c21df1ca91c33d7302d89e4db2788ed6040359
Antony Syriac <unbleavabl@gmail.com>
2017-01-30 15:13:52 +0530

    First Commit

---
+++ Diamond.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Diamond 
+{
+	public static void main(String[] args)
+	{
+		Scanner u_inp = new Scanner(System.in);
+		System.out.print("Enter n : ");
+		int n = u_inp.nextInt();
+
+		for (int i = n-1; i >= 0; i--)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				System.out.print("\t");
+			}
+			for (int k = 0; k < 2*(n-i)-1; k++)
+			{
+				System.out.print("*\t");
+			}
+			System.out.println("");
+		}
+		for (int i = 1; i < n; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				System.out.print("\t");
+			}
+			for (int k = 0; k < 2*(n-i)-1; k++)
+			{
+				System.out.print("*\t");
+			}
+			System.out.println("");
+		}
+	}
+}
